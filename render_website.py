eb71bb3f22389500d425597bde15fde58177a880
svillors <tntzator9@gmail.com>
2025-04-18 21:51:56 +0500

    Split books list by pages.

--- render_website.py
+++ render_website.py
@@ -17,11 +17,17 @@ template = env.get_template('template.html')
 def on_reload():
     with open('./books/meta_data.json') as raw_books:
         books = json.load(raw_books)
-    template = env.get_template('template.html')
-    chunked_books = chunked(books, 2)
-    rendered_page = template.render(chunked_books=chunked_books)
-    with open('index.html', 'w', encoding="utf8") as file:
-        file.write(rendered_page)
+    chunked_books = list(chunked(books, 20))
+    os.makedirs('pages', exist_ok=True)
+    for number, books in enumerate(chunked_books, start=1):
+        template = env.get_template('template.html')
+        rendered_page = template.render(chunked_books=chunked(books, 2))
+        with open(
+            os.path.join('pages', f'index{number}.html'),
+            'w',
+            encoding="utf8"
+        ) as file:
+            file.write(rendered_page)
     print("file updated")
 
 
@@ -35,6 +41,6 @@ template_path = os.path.join(
 server.watch(template_path, on_reload)
 root_dir = os.path.abspath(os.path.dirname(__file__))
 server.serve(root=root_dir,
-             default_filename='index.html',
+             default_filename=os.path.join('pages', 'index1.html'),
              port=8000,
              liveport=35729)
